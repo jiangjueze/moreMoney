486214f9d831735637a7202eb0419c276774a097
jiangjueze <931914369@qq.com>
2023-05-03 21:28:12 +0800

    [feat]money组件css和html+模块化

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`   ">
       <slot/>
     </div>
-    <Nav />
+    <Nav/>
   </div>
 </template>
 
+<script lang="ts">
+  export default {
+    props: ['classPrefix'],
+    name: 'Layout'
+  };
+</script>
+
 <style lang="scss" scoped>
-  .nav-wrapper{
+  .layout-wrapper {
     display: flex;
     flex-direction: column;
     height: 100vh;
   }
-
-  .content{
+  .content {
     overflow: auto;
     flex-grow: 1;
   }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
-    <Layout>
-      <p>Money.vue</p>
-    </Layout>
-  </div>
+  <Layout class-prefix="layout">
+    <NumberPad/>
+    <Types/>
+    <Notes/>
+    <Tags/>
+  </Layout>
 </template>
 
-<script>
+<script lang="ts">
+  import NumberPad from '@/components/Money/NumberPad.vue';
+  import Types from '@/components/Money/Types.vue';
+  import Notes from '@/components/Money/Notes.vue';
+  import Tags from '@/components/Money/Tags.vue';
 
   export default {
-    
-  }
+    name: 'Money',
+    components: {Tags, Notes, Types, NumberPad},
+  };
 </script>
 
-<style lang="scss" scoped>
-  
+<style lang="scss">
+  .layout-content {
+    display: flex;
+    flex-direction: column-reverse;
+  }
 </style>
+
